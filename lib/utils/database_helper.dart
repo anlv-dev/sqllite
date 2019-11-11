4c8df0bd03e6712b7e12b02be1427b30a5f50097
Le An <anlv.it.vn@gmail.com>
2019-11-11 16:13:02 +0700

    add DatabaseHelper

---
+++ lib/utils/database_helper.dart
@@ -0,0 +1,8 @@
+import 'package:sqflite/sql.dart';
+import 'dart:io';
+import 'dart:async';
+import 'package:path_provider/path_provider.dart';
+
+class DatabaseHelper {
+  static DatabaseHelper _databaseHelper; //Singleton Database
+}
